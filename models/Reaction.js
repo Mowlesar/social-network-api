70879998152a4754e65bb53a1b726eee99e85f92
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-10-21 21:15:34 -0400

    Testing and fixing bugs.

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,34 +1,32 @@
 const { Schema, Types, } = require('mongoose');
-const moment = require('moment');
 
 const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default:  () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxLength: 280,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: timestamp => moment(timestamp).format("MMM DD, YYYY [at] hh:mm a"),
-        },
-},
-{
-    toJSON: {
-        virtuals: true,
-        getters: true,
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    id:false,
-}
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
+// const Reaction = model('Reaction', reactionSchema);
 module.exports = reactionSchema;
